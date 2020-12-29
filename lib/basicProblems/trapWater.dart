fce8d0d57e9965eac8d0824caaea3ed014bc4245
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2020-12-29 23:10:20 +0600

    basicProblem

---
+++ lib/basicProblems/trapWater.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+int findTotalWater(arr, n) {
+  int totalWater = 0;
+
+  /// 1st and last index can't contain water
+  for (int i = 1; i < n - 1; i++) {
+    int leffMax = findLeftMax(arr, n, i);
+    int rightMax = findRightMax(arr, n, i);
+
+    totalWater += min(leffMax, rightMax) - arr[i];
+  }
+  return totalWater;
+}
+
+int findLeftMax(arr, n, i) {
+  int maximum = 0;
+  for (int j = 0; j <= i; j++) {
+    maximum = max(maximum, arr[j]);
+  }
+  return maximum;
+}
+
+int findRightMax(arr, n, i) {
+  int maximum = 0;
+  for (int j = n - 1; j >= i; j--) {
+    maximum = max(maximum, arr[j]);
+  }
+  return maximum;
+}
+
+main(List<String> args) {
+  var arr = [2,3,4,5];
+  print(findTotalWater(arr, arr.length));
+}
