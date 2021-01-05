9f2643b4c40e8a435bbcce61d8745905e43eb6cd
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-01-05 14:58:12 +0600

    camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:camera/camera.dart';
 import 'package:dartAdvanced/flutter/map/flutter_map.dart';
+import 'package:dartAdvanced/flutter/phone_integration/camera/camera_widget.dart';
 import 'package:dartAdvanced/flutter/phone_integration/contact_service.dart';
 import 'package:dartAdvanced/flutter/phone_integration/permission_handler.dart';
 import 'package:dartAdvanced/flutter/phone_integration/url_launcher_.dart';
@@ -6,12 +8,18 @@ import 'package:flutter/material.dart';
 
 import 'flutter/animation/animated_logo.dart';
 
-void main() {
+/// `for camera, this is under development, maybe later I'll add save funtion`
+/// `For just Taking picture we can use image_picker https://pub.dev/packages/image_picker `
+List<CameraDescription> cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: new MyApp(),
+    home: MyApp(),
   ));
 }
+
 
 class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
@@ -29,7 +37,8 @@ class _MyAppState extends State<MyApp> {
       // body: OpenStreet(),
       // body: UrlLuncher(),
       // body: PermissionHandler(),
-      body: ContactService(),
+      // body: ContactService(),
+      body: CameraApp(),
     ));
   }
 }
