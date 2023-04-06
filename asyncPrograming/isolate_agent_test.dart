5a2d735fdef0c9e41523d7486c17820046a0d37a
Md. Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2023-04-07 01:20:19 +0600

    Create isolate_agent_test.dart

---
+++ asyncPrograming/isolate_agent_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:isolate_agents/isolate_agents.dart';
+
+int computationallyExpensiveTask([int value = 9999999999]) {
+  var sum = 0;
+  for (var i = 0; i <= value; i++) {
+    sum += i;
+  }
+  print('finished');
+  return sum;
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: BodyWidget(),
+      ),
+    );
+  }
+}
+
+final Future<Agent<int>> _agent = Agent.create(() => 0);
+
+class BodyWidget extends StatefulWidget {
+  @override
+  State<BodyWidget> createState() => _BodyWidgetState();
+}
+
+class _BodyWidgetState extends State<BodyWidget> {
+  int counter = 0;
+
+  Future<int> data() async {
+    Agent<int> agent = await _agent;
+
+    agent.update((p0) => computationallyExpensiveTask());
+    return agent.read();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          CircularProgressIndicator(),
+          ElevatedButton(
+            child: Text('start $counter'),
+            onPressed: () async {
+              final r = await data();
+              counter = r;
+              setState(() {});
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
